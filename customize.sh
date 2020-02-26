78a73fdb195716071828604d036e3cc2d1db82d7
Napoléon Bonaparte <ximliu0228aicmz@gmail.com>
2020-02-26 23:33:10 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,5 +7,6 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.100.18/g' openwrt/package/base-files/files/bin/config_generate
+git clone https://github.com/jerrykuku/lua-maxminddb.git
 git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
 git clone https://github.com/pymumu/smartdns.git package/smartdns
