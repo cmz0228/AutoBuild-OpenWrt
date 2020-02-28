cc348e86670c58aaf94e63baf237f3b63669afb6
Napoléon Bonaparte <ximliu0228aicmz@gmail.com>
2020-02-28 21:40:44 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,6 +7,6 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.100.18/g' openwrt/package/base-files/files/bin/config_generate
-git clone https://github.com/Leo-Jo/luci-app-vssr-coexist package/luci-app-vssr-coexist
+#git clone https://github.com/Leo-Jo/luci-app-vssr-coexist package/luci-app-vssr-coexist
 git clone https://github.com/pymumu/smartdns.git package/smartdns
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
