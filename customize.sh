3ff344287cf567e64a41dc5460731f34678787f3
Napoléon Bonaparte <ximliu0228aicmz@gmail.com>
2020-02-29 16:14:50 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -10,3 +10,4 @@ sed -i 's/192.168.1.1/192.168.100.18/g' openwrt/package/base-files/files/bin/con
 #git clone https://github.com/Leo-Jo/luci-app-vssr-coexist package/luci-app-vssr-coexist
 git clone https://github.com/pymumu/smartdns.git package/smartdns
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+git clone https://github.com/rosywrt/luci-theme-rosy.git
